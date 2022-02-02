78701ed369d12c7e122853afba6a39801ecf7381
Theo-Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-02-01 23:14:12 -0500

    fix?

--- EventRegistration-Backend/src/main/java/ca/mcgil/ecse321/eventregistration/EventRegistrationBackendApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgil/ecse321/eventregistration/EventRegistrationBackendApplication.java
@@ -13,6 +13,7 @@ public class EventRegistrationBackendApplication {
     SpringApplication.run(EventRegistrationBackendApplication.class, args);
   }
 
+  //Request Map
   @RequestMapping("/")
   public String greeting(){
     return "Hello world!";
